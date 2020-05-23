238b579b4a97e6d18236824da21cc831f3e874af
kumarvit <64014824+kumarvit@users.noreply.github.com>
2020-05-23 23:15:40 +0530

    update nsg

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,6 +1,6 @@
 module "vnet" {
-  source = "github.com/tietoevry-infra-as-code/terraform-azurerm-vnet?ref=v1.1.0"
-
+  //source = "github.com/kumarvit/terraform-azurerm-vnet"
+  source = "../.."
   # Using Custom names and VNet/subnet Address Prefix (Recommended)
   create_resource_group = false
   resource_group_name   = "rg-demo-westeurope-01"
@@ -25,14 +25,17 @@ module "vnet" {
         }
       }
       nsg_inbound_rules = [
-        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix]
-        ["weballow", "100", "Inbound", "Allow", "Tcp", "80", "*"],
-        ["weballow1", "101", "Inbound", "Allow", "Tcp", "443", "*"],
-        ["weballow2", "102", "Inbound", "Allow", "Tcp", "8080-8090", "*"],
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix, description]
+        # To use defaults, use "" without adding any values. (Applicable to destination_port_range, source_address_prefix, destination_address_prefix, description).
+        ["weballow", "100", "Inbound", "Allow", "Tcp", "80", "*", "0.0.0.0/0", "http_80"],
+        ["weballow1", "101", "Inbound", "Allow", "", "443", "*", "", "https_443"],
+        ["weballow2", "102", "Inbound", "Allow", "Tcp", "8080-8090", "*", ""],
       ]
 
       nsg_outbound_rules = [
-        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix]
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix, description]
+        # To use defaults, use "" without adding any values. (Applicable to destination_port_range, source_address_prefix, destination_address_prefix, description).
+        ["ntp_out", "103", "Outbound", "Allow", "Udp", "123", "", "0.0.0.0/0", ""],
       ]
     }
 
@@ -42,14 +45,16 @@ module "vnet" {
       service_endpoints     = ["Microsoft.Storage"]
 
       nsg_inbound_rules = [
-        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix]
-        ["weballow", "100", "Inbound", "Allow", "Tcp", "80", "*"],
-        ["weballow1", "101", "Inbound", "Allow", "Tcp", "443", "AzureLoadBalancer"],
-        ["weballow2", "102", "Inbound", "Allow", "Tcp", "9090", "VirtualNetwork"],
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix, description]
+        # To use defaults, use "" without adding any values. (Applicable to destination_port_range, source_address_prefix, destination_address_prefix, description).
+        ["weballow", "100", "Inbound", "Allow", "Tcp", "80", "*", "", "http_80"],
+        ["weballow1", "101", "Inbound", "Allow", "Tcp", "443", "AzureLoadBalancer", "", "https_443"],
+        ["weballow2", "102", "Inbound", "Allow", "Tcp", "9090", "VirtualNetwork", "", "http_9090"],
       ]
 
       nsg_outbound_rules = [
-        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix]
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix, description]
+        # To use defaults, use "" without adding any values. (Applicable to destination_port_range, source_address_prefix, destination_address_prefix, description).
       ]
     }
   }
